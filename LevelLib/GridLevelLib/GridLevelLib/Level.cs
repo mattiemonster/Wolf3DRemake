5a6e5fafcf44a88ed40d3bc08ab8afa0d1635445
Mattie <mattbware1@gmail.com>
2019-06-24 21:28:18 +0100

    Level serialisation and colours

--- LevelLib/GridLevelLib/GridLevelLib/Level.cs
+++ LevelLib/GridLevelLib/GridLevelLib/Level.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
 
 namespace GridLevelLib
 {
@@ -14,10 +11,13 @@ namespace GridLevelLib
         /// Tile value of -1 is an empty tile.
         /// </summary>
         public int[,] tiles;
+        public Color floorColour, ceilingColour;
 
-        public Level(int sizeX, int sizeY)
+        public Level(int sizeX, int sizeY, Color floor, Color ceiling)
         {
             tiles = new int[sizeX, sizeY];
+            floorColour = floor;
+            ceilingColour = ceiling;
 
             // Set all tiles to an empty tile
             for (int k = 0; k < tiles.GetLength(0); k++)
@@ -33,6 +33,45 @@ namespace GridLevelLib
         public void ResetTile(int tileX, int tileY)
         {
             tiles[tileX, tileY] = -1; // Tile becomes empty
+        }
+
+        public void SaveLevel(string path)
+        {
+            XmlLoader.SerializeObject(this, path);
+        }
+
+        public void LoadLevel(string path)
+        {
+            Level loadedLevel = XmlLoader.DeSerializeObject<Level>(path);
+
+            tiles = loadedLevel.tiles;
+            floorColour = loadedLevel.floorColour;
+            ceilingColour = loadedLevel.ceilingColour;
+        }
+
+        public Level LoadAndGetLevel(string path)
+        {
+            return XmlLoader.DeSerializeObject<Level>(path);
+        }
+
+        public Color GetFloorColour()
+        {
+            return floorColour;
+        }
+
+        public void SetFloorColour(Color newColour)
+        {
+            floorColour = newColour;
+        }
+
+        public Color GetCeilingColour()
+        {
+            return ceilingColour;
+        }
+
+        public void SetCeilingColour(Color newColour)
+        {
+            ceilingColour = newColour;
         }
     }
 }
