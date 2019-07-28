df59ac886dcb5b2e9f7f6df87e7d6545cade49a0
Mattie <mattbware1@gmail.com>
2019-07-28 03:24:50 +0100

    Added basic debug UI, changed tile selection UI scale

---
+++ Wolf3DRemake/Assets/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class LevelManager : MonoBehaviour
+{
+    [Header("Scene References")]
+    public LevelLoader loader;
+    public GameObject debugUI;
+    public TextMeshProUGUI levelName;
+    public TextMeshProUGUI tileCount;
+
+    void Start()
+    {
+        debugUI.SetActive(false);
+        levelName.text = LevelLoader.levelToLoad;
+        tileCount.text = "Tile count: " + loader.tiles.Count.ToString();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+            debugUI.SetActive(!debugUI.activeSelf);
+    }
+
+}
