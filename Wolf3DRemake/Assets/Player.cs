b17362f38b44b934ca65e7bfd1ce6d9987f5f0a8
Mattie <mattbware1@gmail.com>
2019-08-27 18:07:46 +0100

    Start combat

---
+++ Wolf3DRemake/Assets/Player.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    bool lmb = false;
+
+    public float gunDistance = 200f;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            lmb = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (lmb)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, gunDistance))
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward), Color.yellow, 5f);
+                if (hit.transform.gameObject.tag == "Enemy")
+                {
+                    Debug.Log("Hit an enemy!");
+                }
+            }
+            lmb = false;
+        }
+    }
+}
